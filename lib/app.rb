fd2d5f78be1e04347c2efd66eddfaa5df786e463
ZeshanRasul <zeshanrasul90@gmail.com>
2016-10-10 21:30:52 +0100

    Added play.erb, added post names method to app, enter names feature passing

--- lib/app.rb
+++ lib/app.rb
@@ -6,6 +6,12 @@ class SuperBattle < Sinatra::Base
     erb :index
   end
 
+  post '/names' do
+    @player_1 = params[:player_1]
+    @player_2 = params[:player_2]
+    erb :play
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 feature 'enter names' do
   scenario 'two users enter their usernames' do
     visit '/'
-    fill_in 'Player 1 name', with: "Bill"
-    fill_in 'Player 2 name', with: "Ted"
+    fill_in 'player_1', with: "Bill"
+    fill_in 'player_2', with: "Ted"
     click_button 'Submit Names'
     expect(page).to have_content "Bill and Ted do battle!"
   end
