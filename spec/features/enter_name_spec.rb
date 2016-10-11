55affe1f2ce787256fcd495e296fbb6cbf2380b7
ZeshanRasul <zeshanrasul90@gmail.com>
2016-10-11 03:00:09 +0100

    Added helper for sign in and play

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper'
 
 feature 'enter names' do
   scenario 'two players enter their names' do
-    visit '/'
-    fill_in 'player_1', with: "Bill"
-    fill_in 'player_2', with: "Ted"
-    click_button 'Submit Names'
+    sign_in_and_play
     expect(page).to have_content "Bill and Ted do battle!"
   end
 end
--- spec/features/view_hitpoints_spec.rb
+++ spec/features/view_hitpoints_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper'
 
 feature 'players can view hitpoints' do
   scenario 'player 1 can view hitpoints of player 2' do
-    visit '/'
-    fill_in 'player_1', with: "Bill"
-    fill_in 'player_2', with: "Ted"
-    click_button 'Submit Names'
+    sign_in_and_play
     expect(page).to have_content "Ted 20hp"
   end
 end
